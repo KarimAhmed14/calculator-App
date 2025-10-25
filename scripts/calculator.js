ce795bcb0112b5cb5754c9b3a73bff3e9d77f888
KarimAhmed14 <kemoalbaqri@gmail.com>
2025-10-25 12:20:11 +0300

    finishing the App

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -8,3 +8,16 @@ function clearDisplay() {
     display.value = "";
 }
 
+function calculateResult() {
+    try {
+        const result = eval(display.value);
+        
+        if (result === Infinity || isNaN(result)) {
+            display.value = "Error";
+        } else {
+            display.value = result;
+        }
+    } catch (error) {
+        display.value = "Error";
+    }
+}
